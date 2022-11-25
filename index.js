2cfb5183728eefb743633e8399629ec67f155469
Grepix Infotech <>
2022-11-25 12:05:32 +0530

    first commit

--- index.js
+++ index.js
@@ -75,25 +75,41 @@
 
 
 
- const jwt = require("jsonwebtoken");
-    const { uuid } = require("uuidv4");
+//  const jwt = require("jsonwebtoken");
+//     const { uuid } = require("uuidv4");
 
-const SEC_KEY = "283eab35d6bd4c91a6986fe6352ce851";
-const PUB_KEY = "73a91b63f1bf4ab7bbf2dabc6358b7c4";
+// const SEC_KEY = "283eab35d6bd4c91a6986fe6352ce851";
+// const PUB_KEY = "73a91b63f1bf4ab7bbf2dabc6358b7c4";
 
-payload = {
-amount: 1,
-currency: "ZMW",
-customerEmail: "mundia@getsparco.com",
-customerFirstName: "Mundia",
-customerLastName: "Mwala",
-customerPhone: "0961453688",
-merchantPublicKey: PUB_KEY,
-transactionName: "Service/Product Name",
-transactionReference: uuid(),
-wallet: "0961453688",
-chargeMe: false,
-};
-const encoded_payload = jwt.sign(payload, SEC_KEY);
+// payload = {
+// amount: 1,
+// currency: "ZMW",
+// customerEmail: "mundia@getsparco.com",
+// customerFirstName: "Mundia",
+// customerLastName: "Mwala",
+// customerPhone: "0961453688",
+// merchantPublicKey: PUB_KEY,
+// transactionName: "Service/Product Name",
+// transactionReference: uuid(),
+// wallet: "0961453688",
+// chargeMe: false,
+// };
+// const encoded_payload = jwt.sign(payload, SEC_KEY);
 
-console.log(encoded_payload);
+// console.log(encoded_payload);
+
+
+
+
+
+
+
+
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080 
